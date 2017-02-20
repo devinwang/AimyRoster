524a262315bdb9fe049c6f01b6477deb7e2c3662
Devin <Devin@MOONW10>
2017-02-21 10:11:02 +1300

    Add project files.

---
+++ AimyRoster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AimyRoster.Startup))]
+namespace AimyRoster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
